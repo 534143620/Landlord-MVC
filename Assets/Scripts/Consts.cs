1a392695fe8c41f89b1a4a269653f74d542717f2
LAPTOP-05DDO7UI\lenovo <534143620@qq.com>
2021-07-11 21:42:38 +0800

    【代码提交】+【牌类数据分析+一些枚举变量的定义】

--- Assets/Scripts/Consts.cs
+++ Assets/Scripts/Consts.cs
@@ -19,3 +19,76 @@ public enum ViewEvent
 {
     ChangeMulitiple
 }
+
+/// <summary>
+/// 持有牌的角色
+/// </summary>
+public enum CharacterType
+{
+    Library,
+    Player,
+    ComputerLeft,
+    ComputerRight,
+    Desk
+}
+
+/// <summary>
+/// 牌的花色
+/// </summary>
+public enum Colors
+{
+    None,   //小王 大王
+    Club,   //梅花 
+    Heart,  //红桃
+    Spade,  //黑桃
+    Square  //方块    
+}
+
+/// <summary>
+/// 牌的大小
+/// </summary>
+public enum Weight 
+{
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    One,
+    Two,
+    SJoker,
+    LJoker
+}
+
+/// <summary>
+/// 牌的类型
+/// </summary>
+public enum CardType
+{
+    Single,     //单
+    Double,     //对
+    Straight,   //顺子
+    DoubleStraight,   //双顺   55 66 
+    TripleStraight,   //飞机  555666
+    Three,            //三不带  333
+    ThreeAndOne,      //三带一  3331
+    ThreeAndTwo,      //三带二  33311
+    Boom,             //炸弹 
+    JokerBoom         //王炸
+
+}
+
+/// <summary>
+/// 牌的身份
+/// </summary>
+public enum Identity
+{
+    Farmer,
+    Landlord
+}
