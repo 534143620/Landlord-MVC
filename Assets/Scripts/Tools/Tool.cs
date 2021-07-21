e58f589096189db645f43eb1b485041f554ff6a9
LAPTOP-05DDO7UI\lenovo <534143620@qq.com>
2021-07-21 21:49:10 +0800

    【代码提交】+【获取牌的大小+对牌堆进行排序】

--- Assets/Scripts/Tools/Tool.cs
+++ Assets/Scripts/Tools/Tool.cs
@@ -22,7 +22,7 @@ public static class Tool
     /// <param name="type">面板类型</param>
     /// <returns></returns>
     public static GameObject CreateUIPanel(PanelType type)
-   {
+    {
         GameObject go = Resources.Load<GameObject>(type.ToString());
         if(go == null)
         {
@@ -36,5 +36,58 @@ public static class Tool
             panel.transform.SetParent(UiParent,false);
             return go;
         }
-   }
+    }
+
+    ///如何对类进行简单的排序
+    /// <summary>
+    /// 排序  
+    /// </summary>
+    /// <param name="cards">要排序的牌</param>
+    /// <param name="asc">升序</param>
+    public static void Sort(List<Card>cards,bool asc)
+    {
+        cards.Sort((Card a,Card b) => 
+        {
+            if (asc)
+                return a.Cardweight.CompareTo(b.Cardweight);
+            else
+                return -a.Cardweight.CompareTo(b.Cardweight);
+        });
+    }
+
+    //如何获取牌的大小，此处并不是很懂，感觉有BUG后续
+    /// <summary>
+    /// 获取牌的大小
+    /// </summary>
+    /// <param name="cards">出的牌</param>
+    /// <param name="cardType">出牌类型</param>
+    /// <returns></returns>
+    public static int GetWegiht(List<Card>cards,CardType cardType)
+    {
+        int totalWeight = 0;
+
+        if(cardType == CardType.ThreeAndOne || cardType == CardType.ThreeAndTwo)
+        {
+            for (int i = 0; i < cards.Count; i++)
+            {
+                if (cards[i].Cardweight == cards[i + 1].Cardweight && cards[i].Cardweight == cards[i + 2].Cardweight)
+                {
+                    totalWeight += (int)cards[i].Cardweight;
+                    totalWeight *= 3;
+                    break;
+                }
+            }
+        }else if (cardType == CardType.Straight || cardType == CardType.DoubleStraight)
+        {
+            totalWeight = (int)cards[0].Cardweight;
+        }
+        else
+        {
+            for (int i = 0; i < cards.Count; i++)
+            {
+                totalWeight += (int)cards[i].Cardweight;
+            }
+        }
+        return totalWeight;
+    }
 }
